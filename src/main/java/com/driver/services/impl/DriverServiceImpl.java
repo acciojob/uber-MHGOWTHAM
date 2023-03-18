1bb63daa00f58300fe8aceaf066bf32ae2fede78
GOWTHAM M H <gowthammh02@gmail.com>
2023-03-18 20:05:15 +0530

    uber

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -1,8 +1,10 @@
 package com.driver.services.impl;
 
 import com.driver.model.Cab;
+import com.driver.model.TripStatus;
 import com.driver.repository.CabRepository;
 import com.driver.services.DriverService;
+import com.zaxxer.hikari.util.DriverDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,18 +23,29 @@ public class DriverServiceImpl implements DriverService {
 	@Override
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+		Driver driver=new Driver();
+		Cab cab=new Cab();
+		driver.setMobile(mobile);
+		driver.setPassword(password);
+		driver.setCab(cab);
 
+		cab.setPerKmRate(10);
+		cab.setAvailable(true);
+		driverRepository3.save(driver);
 	}
 
 	@Override
 	public void removeDriver(int driverId){
 		// Delete driver without using deleteById function
-
+		Driver driver=driverRepository3.findById(driverId).get();
+		driverRepository3.delete(driver);
 	}
 
 	@Override
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
-
+		Driver driver=driverRepository3.findById(driverId).get();
+		driver.getCab().setAvailable(false);
+		driverRepository3.save(driver);
 	}
 }
